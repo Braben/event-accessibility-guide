0de95fbedb546adfe898aca75b5c189268ded8d7
PEAIDOO <alsweleinstein@gmail.com>
2025-03-15 20:34:21 +0000

    working on ui plugins

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Button } from "@material-tailwind/react";
-import AddEvent from "../components/AddEvent";
+// import { Button } from "@material-tailwind/react";
+import { Button } from "flowbite-react";
 
 const Home = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <p className="font-bold italic">Hello</p>
+        {/* <Button>Button</Button> */}
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 };
