57cef0ba7d6cffc5cc6b066d6d34b1812b6ba685
PEAIDOO <alsweleinstein@gmail.com>
2025-03-15 12:14:09 +0000

    downgraded tailwind version and installed material UI

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { Button } from "@material-tailwind/react";
 import AddEvent from "../components/AddEvent";
 
 const Home = () => {
   return (
     <div>
-      <AddEvent />
+      <div>
+        <p className="font-bold italic">Heloloo</p>
+        <Button>Button</Button>
+        <Button>Button</Button>
+      </div>
     </div>
   );
 };
