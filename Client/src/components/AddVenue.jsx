0239b2e654b7ac33e3dd3218119dd448162f63d6
PEAIDOO <alsweleinstein@gmail.com>
2025-03-17 08:53:07 +0000

    filters

---
+++ Client/src/components/AddVenue.jsx
@@ -0,0 +1,209 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Dialog,
+  Button,
+  Input,
+  Checkbox,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { Xmark } from "iconoir-react";
+
+const AddVenue = () => {
+  const [venueName, setVenueName] = useState("");
+  const [venueCapacity, setVenueCapacity] = useState(0);
+  const [venueAddress, setVenueAddress] = useState("");
+  const [venueImage, setVenueImage] = useState(null);
+  const [venueDescription, setVenueDescription] = useState("");
+  const [accessibilityFeatures, setAccessibilityFeatures] = useState([]);
+  const accessibilityOptions = [
+    "Balanced sound",
+    "Earing aids",
+    "Sign language translators",
+    "Braille itinerary",
+    "Wheel chair access",
+    "Elevator",
+    "Automatic doors",
+    "Lighting considerations",
+    "Close parking",
+    "Large prints",
+    "Service Animal Accommodation",
+    "Sensory-Friendly Environment",
+  ];
+
+  const handleAccessibilityChange = (option) => {
+    setAccessibilityFeatures(
+      (prev) =>
+        prev.includes(option)
+          ? prev.filter((item) => item !== option) // Remove if already selected
+          : [...prev, option] // Add if not selected
+    );
+  };
+  return (
+    <div>
+      <Dialog size="md">
+        <Dialog.Trigger
+          className="bg-white text-black font-bold w-70 hover:bg-white rounded-none"
+          as={Button}
+        >
+          + Add Venue
+        </Dialog.Trigger>
+        <Dialog.Overlay>
+          <Dialog.Content>
+            <Dialog.DismissTrigger
+              as={IconButton}
+              size="sm"
+              variant="ghost"
+              color="secondary"
+              className="absolute right-2 top-2"
+              isCircular
+            >
+              <Xmark className="h-5 w-5" />
+            </Dialog.DismissTrigger>
+            <Typography type="h6" className="mb-1">
+              + Add New Venue
+            </Typography>
+            <Typography className="text-foreground">
+              Add a new venue with Accessibility information
+            </Typography>
+            <form action="#" className="mt-3">
+              <div className="mb-4 mt-2 space-y-1.5">
+                <div className="flex justify-between gap-4">
+                  {/* Left Input Field */}
+                  <div className="w-1/2">
+                    <Typography
+                      as="label"
+                      htmlFor="text1"
+                      type="small"
+                      color="default"
+                      className="font-semibold"
+                    >
+                      Venue Name
+                    </Typography>
+                    <Input
+                      id="text1"
+                      type="text"
+                      value={venueName}
+                      placeholder="eg: national theater"
+                      className="w-full"
+                      onChange={(e) => setVenueName(e.target.value)}
+                    />
+                  </div>
+
+                  {/* Right Input Field */}
+                  <div className="w-1/2">
+                    <Typography
+                      as="label"
+                      htmlFor="text2"
+                      type="small"
+                      color="default"
+                      className="font-semibold"
+                    >
+                      Capacity
+                    </Typography>
+                    <Input
+                      id="text2"
+                      type="number"
+                      value={venueCapacity}
+                      placeholder="eg: 300"
+                      className="w-full"
+                      onChange={(e) => setVenueCapacity(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 space-y-1.5">
+                <Typography
+                  as="label"
+                  htmlFor="text3"
+                  type="small"
+                  color="default"
+                  className="font-semibold"
+                >
+                  Address
+                </Typography>
+                <Input
+                  id="text3"
+                  type="text"
+                  value={venueAddress}
+                  onChange={(e) => setVenueAddress(e.target.value)}
+                  placeholder="eg: 123 main st, Accra, Ghana "
+                />
+              </div>
+
+              <div className="mb-4 space-y-1.5">
+                <Typography
+                  as="label"
+                  htmlFor="file"
+                  type="small"
+                  color="default"
+                  className="font-semibold"
+                >
+                  Add Venue Image
+                </Typography>
+                <input
+                  id="file"
+                  type="file"
+                  className="file-input file-input-primary w-full"
+                  required
+                  accept="image/jpeg, image/jpg"
+                  onChange={(e) => setVenueImage(e.target.files[0])}
+                />
+              </div>
+
+              <div className="mb-4 space-y-1.5">
+                <Typography
+                  as="label"
+                  htmlFor="text3"
+                  type="small"
+                  color="default"
+                  className="font-semibold"
+                >
+                  Description
+                </Typography>
+                <Input
+                  id="text3"
+                  type="text"
+                  value={venueDescription}
+                  onChange={(e) => setVenueDescription(e.target.value)}
+                  placeholder="typing... "
+                />
+              </div>
+
+              <div className="mt-6">
+                <h4 className="text-lg font-semibold">
+                  Accessibility Features
+                </h4>
+                <div className="grid grid-cols-2 gap-2 mt-2">
+                  {accessibilityOptions.map((feature) => (
+                    <label
+                      key={feature}
+                      className="flex items-center space-x-2"
+                    >
+                      <input
+                        type="checkbox"
+                        className="checkbox"
+                        checked={accessibilityFeatures.includes(feature)}
+                        onChange={() => handleAccessibilityChange(feature)}
+                      />
+                      <span>{feature}</span>
+                    </label>
+                  ))}
+                </div>
+              </div>
+            </form>
+            <div className="mb-1 flex items-center mt-3 justify-end gap-2">
+              <Dialog.DismissTrigger as={Button} variant="ghost" color="error">
+                Cancel
+              </Dialog.DismissTrigger>
+              <Button type="submit">Add Event</Button>
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddVenue;
--- Client/src/pages/Events.jsx
+++ Client/src/pages/Events.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import { Input } from "@material-tailwind/react";
+import { IoMdSearch } from "react-icons/io";
+import { CiFilter } from "react-icons/ci";
+import { Button } from "@material-tailwind/react";
+import { useState } from "react";
 const Events = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [showFilters, setShowFilters] = useState(false);
 
-export default Events
+  return (
+    <div className="mx-8 ">
+      <div className="mt-4 ">
+        <h3 className="font-bold text-2xl">Accessible Venues</h3>
+        <p>Find venues with accessibility features for your next event </p>
+      </div>
+      <div className="flex items-center gap-3 ">
+        <form
+          className="bg-gray-200 h-12 w-4/5 gap-3 px-5 flex items-center justify-center border border-black border-opacity-40"
+          // onSubmit={handleSearch}
+        >
+          <button type="submit" className="transition">
+            <IoMdSearch className="size-8 font-bold text-gray-700" />
+          </button>
+
+          <Input
+            className="border-transparent focus:ring-0 focus:outline-none focus:border-transparent focus-visible:ring-0 focus-visible:outline-none shadow-none placeholder:text-black  bg-transparent w-64 p-0"
+            color="secondary"
+            placeholder="Search venues"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault(); // Prevent default form submission
+                handleSearch(); // Call search function
+              }
+            }}
+            style={{
+              border: "none",
+              outline: "none",
+              boxShadow: "none",
+            }}
+          />
+        </form>
+
+        <button
+          className="btn btn-outline border-black w-1/4  border-opacity-40  focus:outline-none focus:ring-2 hover:bg-gray-300  h-12"
+          style={{
+            outline: "none",
+            boxShadow: "none",
+          }}
+          onClick={() => setShowFilters(!showFilters)}
+        >
+          <CiFilter className="size-6  stroke-[1]" />
+          {showFilters ? "Hide Filters" : "Show Filters"}
+        </button>
+      </div>
+
+      {showFilters && (
+        <div id="filters" className="h-48 w-full  bg-slate-950 mt-2"></div>
+      )}
+    </div>
+  );
+};
+
+export default Events;
