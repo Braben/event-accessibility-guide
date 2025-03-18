484291ee4e8ab1832a5c73fd925bfec9a8d6dd4f
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2025-03-18 13:59:50 +0000

    filter focus done

--- Client/src/pages/Events.jsx
+++ Client/src/pages/Events.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@material-tailwind/react";
 import { IoMdSearch } from "react-icons/io";
 import { CiFilter } from "react-icons/ci";
 import { Button } from "@material-tailwind/react";
-import { useState } from "react";
 const Events = () => {
   const [showFilters, setShowFilters] = useState(false);
+  const [filters, setFilters] = useState({
+    location: "",
+    accessibility: "",
+  });
+  const [venues, setVenues] = useState([]);
+
+  //function to handle search/filter
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      // fetch api from backend
+      const response = await fetch();
+      const data = await response.json();
+      // store filtered data
+      setVenues(data.venue);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
 
   return (
     <div className="mx-8 ">
@@ -16,7 +35,7 @@ const Events = () => {
       <div className="flex items-center gap-3 ">
         <form
           className="bg-gray-200 h-12 w-4/5 gap-3 px-5 flex items-center justify-center border border-black border-opacity-40"
-          // onSubmit={handleSearch}
+          onSubmit={handleSearch}
         >
           <button type="submit" className="transition">
             <IoMdSearch className="size-8 font-bold text-gray-700" />
@@ -26,6 +45,8 @@ const Events = () => {
             className="border-transparent focus:ring-0 focus:outline-none focus:border-transparent focus-visible:ring-0 focus-visible:outline-none shadow-none placeholder:text-black  bg-transparent w-64 p-0"
             color="secondary"
             placeholder="Search venues"
+            value={filters.location}
+            onChange={(e) => setFilters({ ...filters, location: e.target.value })}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
                 e.preventDefault(); // Prevent default form submission
@@ -54,7 +75,32 @@ const Events = () => {
       </div>
 
       {showFilters && (
-        <div id="filters" className="h-48 w-full  bg-slate-950 mt-2"></div>
+        <div id="filters" className="h-48 w-full  bg-slate-950 mt-2">
+          <label>Location:</label>
+          <Input className="text-white"
+            type="text"
+            placeholder="Enter location"
+            value={filters.location}
+            onChange={(e) =>
+              setFilters({...filters, location: e.target.value })}
+          />
+          <label>Accessibility Features:</label>
+          <select value={filters.accessibility} onChange={(e) => setFilters({...filters, accessibility: e.target.value})}>
+            <option value="">Select All</option>
+            <option value="wheelchair accessible">Wheelchair Accessible</option>
+            <option value="accessible seating">Accessible Seating</option>
+            <option value="visual aids">Visual Aids</option>
+            <option value="braille">Braille</option>
+            <option value="sign language interpreters">Sign Language Interpreters</option>
+            <option value="hearing aids">Hearing Aids</option>
+            <option value="speech-to-text assistants">Speech-to-Text Assistants</option>
+            <option value="deafness support">Deafness Support</option>
+            <option value="wheelchair-accessible restrooms">
+              Wheelchair-Accessible Restrooms
+            </option>
+            <option value="noisy environments">Noisy Environments</option>
+          </select>
+        </div>
       )}
     </div>
   );
