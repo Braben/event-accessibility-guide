1d74cf8ccab06a647ceefa52e5f3e265db000ac4
Benjamin Lamptey <kbenlamptey@gmail.com>
2025-06-11 19:45:53 +0000

    fixing routes

--- Server/Routes/accessibilityFeaturesRoutes.js
+++ Server/Routes/accessibilityFeaturesRoutes.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const { accessibilityFeatures } = require("../middlewares/validation")
+const { accessibilityFeatures } = require("../middlewares/validation");
 const {
-  createFeature, getAllFeatures, getFeatureById, updateFeature,deleteFeature
-} = require("../controllers/accessibiityFeaturesController")
+  createFeature,
+  getAllFeatures,
+  getFeatureById,
+  updateFeature,
+  deleteFeature,
+} = require("../controllers/accessibiityFeaturesController");
 
 const router = express.Router();
 
-router.post('/', createFeature);            
-router.get('/', getAllFeatures);            
-router.get('/:id', getFeatureById);       
-router.put('/:id', updateFeature);         
-router.delete('/:id', deleteFeature);    
+router.post("/", createFeature);
+router.get("/", getAllFeatures);
+router.get("/:id", getFeatureById);
+router.put("/:id", updateFeature);
+router.delete("/:id", deleteFeature);
 
 module.exports = router;
--- Server/Routes/loginRoutes.js
+++ Server/Routes/loginRoutes.js
@@ -1,12 +1,9 @@
-
 const express = require("express");
 const router = express.Router();
-const {isAuth} = require("../middlewares/isAuth");
+const { isAuth } = require("../middlewares/isAuth");
 const { loginUser, logoutUser } = require("../controllers/loginController");
 
 router.post("/login", loginUser);
 router.post("/logout", isAuth, logoutUser);
 
 module.exports = router;
-
-
