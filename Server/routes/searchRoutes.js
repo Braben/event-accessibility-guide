991a83e9603c853619c5df55a20679748a0e4822
Benjamin Lamptey <kbenlamptey@gmail.com>
2025-06-11 19:00:40 +0000

    fixing userroutes

--- Server/routes/searchRoutes.js
+++ Server/routes/searchRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { searchVenues } = require("../controllers/searchController")
+const { searchVenues } = require("../controllers/searchController");
 
 const router = express.Router();
-router.get("/venues/search", searchVenues); 
+router.get("/venues/search", searchVenues);
 
 module.exports = router;
