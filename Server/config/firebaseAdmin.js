d33b709ab53e8d5a83288a316fe44519bd207617
Benjamin Lamptey <kbenlamptey@gmail.com>
2025-06-11 18:47:59 +0000

    fixing routes for deployment

--- Server/config/firebaseAdmin.js
+++ Server/config/firebaseAdmin.js
@@ -2,8 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const admin = require("firebase-admin");
-console.log("Firebase_Admin_SDK_Credentials:", process.env.Firebase_Admin_SDK_Credentials);
-const serviceAccount = JSON.parse(process.env.Firebase_Admin_SDK_Credentials );
+// console.log("Firebase_Admin_SDK_Credentials:", process.env.Firebase_Admin_SDK_Credentials);
+const serviceAccount = JSON.parse(process.env.Firebase_Admin_SDK_Credentials);
 // Initialize Firebase Admin SDK
 
 admin.initializeApp({
--- Server/server.js
+++ Server/server.js
@@ -5,13 +5,14 @@ dotenv.config();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const userRoute = require("./routes/userRoutes");
+// const userRoute = require("./routes/userRoutes");
+const userRoute = require("../Server/routes/userRoutes");
 const venueRoutes = require("./routes/venueRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 const loginRoute = require("./routes/loginRoutes");
 const profileRoute = require("./routes/profileRoutes");
-const accessibilityFeaturesRoutes = require('./routes/accessibilityFeaturesRoutes');
+const accessibilityFeaturesRoutes = require("./routes/accessibilityFeaturesRoutes");
 const cookieParser = require("cookie-parser");
 // const prisma = require("./prisma");
 
@@ -26,30 +27,32 @@ const allowedOrigins = [
   "http://localhost:5175",
   "http://localhost:5176",
   "http://localhost:5177",
-  "https://venuehubs.netlify.app", 
+  "https://venuehubs.netlify.app",
 ];
 
 // Middleware
 app.use(cookieParser());
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Authorization", "Content-Type"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
 
-app.use(cookieParser());  
+app.use(cookieParser());
 app.use(express.json());
 
 // Connect to routes
-app.use("/venues", venueRoutes)
-app.use("/reviews", reviewRoutes)
-app.use("/api", searchRoutes)
+app.use("/venues", venueRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/api", searchRoutes);
 // app.use(userRoutes);
 app.use("/users", userRoute);
 app.use("/user", loginRoute);
 app.use("/profile", profileRoute);
-app.use("/features",accessibilityFeaturesRoutes);
+app.use("/features", accessibilityFeaturesRoutes);
 // app.use("/auth", loginRoute);
 
 //for firebase
